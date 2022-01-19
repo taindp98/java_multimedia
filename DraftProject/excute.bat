20125b6e060166e14d0d0091675222d1c5e6585d
Tai NGUYEN <taindp98@gmail.com>
2022-01-19 10:55:08 +0100

    refactor

--- DraftProject/excute.bat
+++
@@ -1,7 +0,0 @@
-javac AnalogClock.java
-@REM java AnalogClock
-@REM javac TimeSpinner.java
-@REM javac TimeSpinnerExample.java
-@REM java TimeSpinnerExample
-javac RunAnalogClock.java
-java RunAnalogClock
